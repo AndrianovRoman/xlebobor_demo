8666fd3278c67f7cc60cf358fd3cf7740f478143
Andrianov Roman <r_andrianov02@mail.ru>
2024-12-01 22:35:45 +0300

    update project

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,116 @@
 
 $(document).ready(function(){
 
+    // -------------------------------------------- Код для точки следящей за курсором
+
+    $("body").prepend('<div id="circle" class="circle-cursor"></div>', '<div id="min-circle" class="circle-cursor-min"></div>');
+
+    var cursor = {
+        delay: 10,
+        _x: 0,
+        _y: 0,
+        endX: (window.innerWidth / 2),
+        endY: (window.innerHeight / 2),
+        cursorVisible: true,
+        cursorEnlarged: false,
+        $dot: document.querySelector('#min-circle'),
+        $outline: document.querySelector('#circle'),
+
+        init: function() {
+            this.dotSize = this.$dot.offsetWidth;
+            this.outlineSize = this.$outline.offsetWidth;
+
+            this.setupEventListeners();
+            this.animateDotOutline();
+        },
+
+        setupEventListeners: function() {
+            var self = this;
+
+            document.querySelectorAll('a').forEach(function(el) {
+                el.addEventListener('mouseover', function() {
+                    self.cursorEnlarged = true;
+                    self.toggleCursorSize();
+                });
+                el.addEventListener('mouseout', function() {
+                    self.cursorEnlarged = false;
+                    self.toggleCursorSize();
+                });
+            });
+
+            document.addEventListener('mousedown', function() {
+                self.cursorEnlarged = true;
+                self.toggleCursorSize();
+            });
+            document.addEventListener('mouseup', function() {
+                self.cursorEnlarged = false;
+                self.toggleCursorSize();
+            });
+
+            document.addEventListener('mousemove', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+
+                self.endX = e.clientX;
+                self.endY = e.clientY;
+                self.$dot.style.top = self.endY + 'px';
+                self.$dot.style.left = self.endX + 'px';
+            });
+
+            document.addEventListener('mouseenter', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+                self.$dot.style.opacity = 1;
+                self.$outline.style.opacity = 1;
+            });
+
+            document.addEventListener('mouseleave', function(e) {
+                self.cursorVisible = true;
+                self.toggleCursorVisibility();
+                self.$dot.style.opacity = 0;
+                self.$outline.style.opacity = 0;
+            });
+        },
+
+        animateDotOutline: function() {
+            var self = this;
+
+            self._x += (self.endX - self._x) / self.delay;
+            self._y += (self.endY - self._y) / self.delay;
+            self.$outline.style.top = self._y + 'px';
+            self.$outline.style.left = self._x + 'px';
+
+            requestAnimationFrame(this.animateDotOutline.bind(self));
+        },
+
+        toggleCursorSize: function() {
+            var self = this;
+
+            if (self.cursorEnlarged) {
+                self.$dot.style.transform = "translate(-50%, -50%) scale(3.02)";
+                self.$outline.style.transform = 'translate(-50%, -50%) scale(0)';
+            } else {
+                self.$dot.style.transform = 'translate(-50%, -50%) scale(1)';
+                self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+            }
+        },
+
+        toggleCursorVisibility: function() {
+            var self = this;
+
+            if (self.cursorVisible) {
+                self.$dot.style.opacity = 1;
+                self.$outline.style.opacity = 1;
+            } else {
+                self.$dot.style.opacity = 0;
+                self.$outline.style.opacity = 0;
+            }
+        }
+    }
+    cursor.init();
+
+    // -------------------------------------------- Код для библиотеки wow js
+
     const wow = new WOW(
         {
             boxClass:     'wow',      // animated element css class (default is wow)
@@ -11,6 +121,8 @@ $(document).ready(function(){
         }
     );
     wow.init();
+
+    // -------------------------------------------- Анимация блока о нас
 
     let items = document.querySelectorAll('.about-actions-item');
 
@@ -34,6 +146,8 @@ $(document).ready(function(){
 
         });
     });
+
+    // -------------------------------------------- Карусель для новостей
 
     $('.news-items').not('.slick-initialized').slick({
         infinite: true,
@@ -71,6 +185,8 @@ $(document).ready(function(){
         ]
     });
 
+    // -------------------------------------------- Карусель для партнеров
+
     let toggleSlick = () => {
         if($(window).width() < 620) {
             $('.collaboration-items').not('.slick-initialized').slick({
@@ -88,4 +204,3 @@ $(document).ready(function(){
     toggleSlick();
 
 });
-
